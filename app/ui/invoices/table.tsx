8f0aca95e8f24b3fde19bfde8e62d638dfa8d149
zzh <zihao.zhang@palmpay-inc.com>
2024-05-17 11:35:14 +0800

    update

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -11,6 +11,7 @@ export default async function InvoicesTable({
   query: string;
   currentPage: number;
 }) {
+  console.log('====query:{}', query)
   const invoices = await fetchFilteredInvoices(query, currentPage);
 
   return (
