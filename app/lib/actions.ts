9ce6fcd003e6946101ca21a468ff97f4b30af16d
zzh <zihao.zhang@palmpay-inc.com>
2024-05-22 16:54:29 +0800

    update invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,11 +6,11 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
-    id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
-    date: z.string(),
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({id: true, date: true});
